30462054a7e163d1cb3b911c56a084b330de16cd
Amir Alavi <sir.amir.71@gmail.com>
2021-12-16 20:28:25 +0330

    Login & Register added to project

--- src/containers/Toplearn.jsx
+++ src/containers/Toplearn.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from "react";
 import Courses from "../components/courses/courses";
 import MainLayout from "../components/Layouts/MainLayout";
+import Login from "../components/login/Login";
+import Register from "../components/Register/Register";
 
 const Toplearn = () => {
     return (
         <MainLayout>
             <Courses />
+            <Login/>
+            <Register/>
         </MainLayout>
     );
 };
