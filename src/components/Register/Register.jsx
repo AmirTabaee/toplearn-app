99792e597ef7da8d8768f977b4952eae7d78b6ea
Amir Alavi <sir.amir.71@gmail.com>
2021-12-27 00:46:12 +0330

    register data with axios on server p1

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { toast } from "react-toastify";
-import { registerUser } from "./../../services/userService";
 
 const Register = () => {
     const [fullname, setFullname] = useState("");
@@ -23,40 +23,50 @@ const Register = () => {
             setPassword("");
         };
 
-        try {
-            const { status } = await registerUser(user);
-
-            if (status === 201) {
-                toast.success("شخص با موفقیت اضافه شد", {
-                    position: "top-right",
-                    closeOnClick: true,
-                });
-                reset();
-            }
-        } catch (ex) {
-            toast.error("مشکلی پیش امده", {
-                position: "bottom-right",
-                closeOnClick: true,
-            });
-        }
+        axios
+            .post(
+                "https://toplearnapi.ghorbany.dev/api/register",
+                JSON.stringify(user),
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                }
+            )
+            .then(({ data, status }) => {
+                console.log(data);
+                if (status === 201) {
+                    reset();
+                    toast.success("کاربر با موفقیت ثبت گردید", {
+                        position: "top-right",
+                        closeOnClick: true,
+                    });
+                } else {
+                    toast.error("مشکلی پیش امده", {
+                        position: "bottom-right",
+                        closeOnClick: true,
+                    });
+                }
+            })
+            .catch((er) => console.log(er));
     };
 
     return (
-        <main class="client-page">
-            <div class="container-content">
+        <main className="client-page">
+            <div className="container-content">
                 <header>
                     <h2> عضویت در سایت </h2>
                 </header>
 
-                <div class="form-layer">
+                <div className="form-layer">
                     <form onSubmit={handleSubmit} action="" method="">
-                        <div class="input-group">
-                            <span class="input-group-addon" id="username">
-                                <i class="zmdi zmdi-account"></i>
+                        <div className="input-group">
+                            <span className="input-group-addon" id="username">
+                                <i className="zmdi zmdi-account"></i>
                             </span>
                             <input
                                 type="text"
-                                class="form-control"
+                                className="form-control"
                                 placeholder="نام و نام خانوادگی"
                                 aria-describedby="username"
                                 value={fullname}
@@ -64,13 +74,13 @@ const Register = () => {
                             />
                         </div>
 
-                        <div class="input-group">
-                            <span class="input-group-addon" id="email-address">
-                                <i class="zmdi zmdi-email"></i>
+                        <div className="input-group">
+                            <span className="input-group-addon" id="email-address">
+                                <i className="zmdi zmdi-email"></i>
                             </span>
                             <input
                                 type="text"
-                                class="form-control"
+                                className="form-control"
                                 placeholder="ایمیل"
                                 aria-describedby="email-address"
                                 value={email}
@@ -80,13 +90,13 @@ const Register = () => {
                             />
                         </div>
 
-                        <div class="input-group">
-                            <span class="input-group-addon" id="password">
-                                <i class="zmdi zmdi-lock"></i>
+                        <div className="input-group">
+                            <span className="input-group-addon" id="password">
+                                <i className="zmdi zmdi-lock"></i>
                             </span>
                             <input
                                 type="password"
-                                class="form-control"
+                                className="form-control"
                                 placeholder="رمز عبور "
                                 aria-describedby="password"
                                 value={password}
@@ -94,26 +104,26 @@ const Register = () => {
                             />
                         </div>
 
-                        <div class="accept-rules">
+                        <div className="accept-rules">
                             <label>
                                 <input type="checkbox" name="" /> قوانین و
                                 مقررات سایت را میپذیرم{" "}
                             </label>
                         </div>
 
-                        <div class="link">
+                        <div className="link">
                             <a href="">
                                 {" "}
-                                <i class="zmdi zmdi-assignment"></i> قوانین و
+                                <i className="zmdi zmdi-assignment"></i> قوانین و
                                 مقررات سایت !
                             </a>
                             <a href="">
                                 {" "}
-                                <i class="zmdi zmdi-account"></i> ورود به سایت{" "}
+                                <i className="zmdi zmdi-account"></i> ورود به سایت{" "}
                             </a>
                         </div>
 
-                        <button class="btn btn-success"> عضویت در سایت </button>
+                        <button className="btn btn-success"> عضویت در سایت </button>
                     </form>
                 </div>
             </div>
